d4d284e37b39e6a97322125a30314d53e7072949
Thiago Bernardo <thiagobsn@gmail.com>
2023-10-26 22:44:38 -0300

    Insert status pedido

---
+++ src/main/resources/db/migration/V14__insert-statuspedido.sql
@@ -0,0 +1,4 @@
+INSERT INTO StatusPedido(descricao) VALUES ('Recebido');
+INSERT INTO StatusPedido(descricao) VALUES ('Em preparação');
+INSERT INTO StatusPedido(descricao) VALUES ('Pronto');
+INSERT INTO StatusPedido(descricao) VALUES ('Finalizado');
