9d6ab23b737c2a0fddccfe790270544bbea23748
Thiago Bernardo <thiagobsn@gmail.com>
2024-02-29 23:37:55 -0300

    Ajuste

--- src/test/java/com/fiap/lanchoneteapp/LanchoneteAppApplicationTests.java
+++ src/test/java/com/fiap/lanchoneteapp/LanchoneteAppApplicationTests.java
@@ -3,9 +3,8 @@ package com.fiap.lanchoneteapp;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LanchoneteAppApplicationTests {
 
 	@Test
